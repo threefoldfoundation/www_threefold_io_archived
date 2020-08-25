706c7b0556b60be1076f9687f853facadfaf428c
samaradel <samar@incubaid.com>
2020-08-25 11:24:44 +0200

    Fix testmonials in mobile screens

--- html/src/components/Partners.svelte
+++ html/src/components/Partners.svelte
@@ -79,7 +79,7 @@
         </div>
       </div>
     </div>
-    <div class="row">
+    <div class="row my-5">
       <div class="col-md-4">
         <div class="row">
           <div class="col-md-4">
